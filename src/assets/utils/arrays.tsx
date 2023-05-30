3cdf4f50cb8a4b55fd4c5bf4dd9a4d89bf6d8a57
rodriguezmauricio <contato.mauricioalves@gmail.com>
2023-05-29 21:27:10 -0300

    att titulo e favicon

--- src/assets/utils/arrays.tsx
+++ src/assets/utils/arrays.tsx
@@ -1,134 +1,141 @@
 export const navMenu = [
-	{
-		id: "01/",
-		title: "Home",
-		titlePt: "Início",
-		sectionId: "intro-section",
-	},
-	{
-		id: "02/",
-		title: "About",
-		titlePt: "Sobre",
-		sectionId: "about-section",
-	},
-	{
-		id: "03/",
-		title: "Portfolio",
-		titlePt: "Portfolio",
-		sectionId: "portfolio-section",
-	},
-	{
-		id: "04/",
-		title: "Formation",
-		titlePt: "Formação",
-		sectionId: "formation-section",
-	},
-	{
-		id: "05/",
-		title: "Contact",
-		titlePt: "Contato",
-		sectionId: "contact-section",
-	},
+  {
+    id: "01/",
+    title: "Home",
+    titlePt: "Início",
+    sectionId: "intro-section",
+  },
+  {
+    id: "02/",
+    title: "About",
+    titlePt: "Sobre",
+    sectionId: "about-section",
+  },
+  {
+    id: "03/",
+    title: "Portfolio",
+    titlePt: "Portfolio",
+    sectionId: "portfolio-section",
+  },
+  {
+    id: "04/",
+    title: "Formation",
+    titlePt: "Formação",
+    sectionId: "formation-section",
+  },
+  {
+    id: "05/",
+    title: "Contact",
+    titlePt: "Contato",
+    sectionId: "contact-section",
+  },
 ];
 
 export const formationDev = [
-	{
-		title: "Baschelor in Computer Science",
-		titlePt: "Bacharelado em Ciência da Computação",
-		local: "Estácio de Sá",
-		year: "2023 - 2027",
-	},
-	{
-		title: "The Modern JavaScript Bootcamp",
-		titlePt: "The Modern JavaScript Bootcamp",
-		local: "Udemy",
-		year: 2022,
-	},
-	{
-		title: "ES6 Javascript: The complete Developer’s Guide",
-		titlePt: "ES6 Javascript: The complete Developer’s Guide",
-		local: "Udemy",
-		year: 2022,
-	},
-	{
-		title: "Modern React with Redux [2023 Update]",
-		titlePt: "Modern React with Redux [2023 Update]",
-		local: "Udemy",
-		year: 2022,
-	},
-	{
-		title: "The Complete React Native + Hooks Course",
-		titlePt: "The Complete React Native + Hooks Course",
-		local: "Udemy",
-		year: 2022,
-	},
-	{
-		title: "Typescript - The Complete Developer’s Guide",
-		titlePt: "Typescript - The Complete Developer’s Guide",
-		local: "Udemy",
-		year: 2022,
-	},
+  {
+    title: "Baschelor in Computer Science",
+    titlePt: "Bacharelado em Ciência da Computação",
+    local: "Estácio de Sá",
+    year: "2023 - 2027",
+  },
+  {
+    title: "The Modern JavaScript Bootcamp",
+    titlePt: "The Modern JavaScript Bootcamp",
+    local: "Udemy",
+    year: 2022,
+  },
+  {
+    title: "ES6 Javascript: The complete Developer’s Guide",
+    titlePt: "ES6 Javascript: The complete Developer’s Guide",
+    local: "Udemy",
+    year: 2022,
+  },
+  {
+    title: "Modern React with Redux [2023 Update]",
+    titlePt: "Modern React with Redux [2023 Update]",
+    local: "Udemy",
+    year: 2022,
+  },
+  {
+    title: "The Complete React Native + Hooks Course",
+    titlePt: "The Complete React Native + Hooks Course",
+    local: "Udemy",
+    year: 2022,
+  },
+  {
+    title: "Typescript - The Complete Developer’s Guide",
+    titlePt: "Typescript - The Complete Developer’s Guide",
+    local: "Udemy",
+    year: 2022,
+  },
 ];
 
 export const formationDesign = [
-	{
-		title: "Adobe Certified Expert Photoshop",
-		titlePt: "Adobe Certified Expert Photoshop",
-		local: "Adobe",
-		year: 2010,
-	},
-	{
-		title: "Adobe Certified Expert Illustrator",
-		titlePt: "Adobe Certified Expert Illustrator",
-		local: "Adobe",
-		year: 2010,
-	},
-	{
-		title: "Adobe Certified Expert Indesign",
-		titlePt: "Adobe Certified Expert Indesign",
-		local: "Adobe",
-		year: 2011,
-	},
-	{
-		title: "Adobe Certified Expert Acrobat",
-		titlePt: "Adobe Certified Expert Acrobat",
-		local: "Adobe",
-		year: 2011,
-	},
-	{
-		title: "Adobe Certified Design Specialist",
-		titlePt: "Adobe Certified Design Specialist",
-		local: "Adobe",
-		year: 2011,
-	},
-	{
-		title: "Adobe Certified Design Master",
-		titlePt: "Adobe Certified Design Master",
-		local: "Adobe",
-		year: 2011,
-	},
+  {
+    title: "Adobe Certified Expert Photoshop",
+    titlePt: "Adobe Certified Expert Photoshop",
+    local: "Adobe",
+    year: 2010,
+  },
+  {
+    title: "Adobe Certified Expert Illustrator",
+    titlePt: "Adobe Certified Expert Illustrator",
+    local: "Adobe",
+    year: 2010,
+  },
+  {
+    title: "Adobe Certified Expert Indesign",
+    titlePt: "Adobe Certified Expert Indesign",
+    local: "Adobe",
+    year: 2011,
+  },
+  {
+    title: "Adobe Certified Expert Acrobat",
+    titlePt: "Adobe Certified Expert Acrobat",
+    local: "Adobe",
+    year: 2011,
+  },
+  {
+    title: "Adobe Certified Design Specialist",
+    titlePt: "Adobe Certified Design Specialist",
+    local: "Adobe",
+    year: 2011,
+  },
+  {
+    title: "Adobe Certified Design Master",
+    titlePt: "Adobe Certified Design Master",
+    local: "Adobe",
+    year: 2011,
+  },
 ];
 
 export const portfolioContent = [
-	{
-		projectName: "Ramana Borba (Dancer Influencer)",
-		projectLink: "http://ramanaborbaoficial.com.br/",
-		repositoryLink: "",
-		bgImage: "https://meuwebdesigner.com.br/wp-content/uploads/2023/05/ramana-min.jpg",
-		projectType: "real",
-	},
-	{
-		projectName: "Franciale Melo (Travel Blog)",
-		projectLink: "https://francialemelo.com.br/",
-		repositoryLink: "",
-		bgImage: "https://meuwebdesigner.com.br/wp-content/uploads/2023/05/fran-min-1.jpg",
-		projectType: "real",
-	},
-	{
-		projectName: "Martz Todo list app",
-		projectLink: "https://martz-todo-list-react-01.vercel.app/",
-		repositoryLink: "https://github.com/rodriguezmauricio/martz-todo-list-react",
-		bgImage: "https://meuwebdesigner.com.br/wp-content/uploads/2023/05/todo-list-min.jpg",
-		projectType: "personal",
-	},
+  {
+    projectName: "Ramana Borba (Dancer Influencer)",
+    projectLink: "http://ramanaborbaoficial.com.br/",
+    repositoryLink: "",
+    bgImage: "https://meuwebdesigner.com.br/wp-content/uploads/2023/05/ramana-min.jpg",
+    projectType: "real",
+  },
+  {
+    projectName: "Franciale Melo (Travel Blog)",
+    projectLink: "https://francialemelo.com.br/",
+    repositoryLink: "",
+    bgImage: "https://meuwebdesigner.com.br/wp-content/uploads/2023/05/fran-min-1.jpg",
+    projectType: "real",
+  },
+  {
+    projectName: "Martz Todo list app",
+    projectLink: "https://martz-todo-list-react-01.vercel.app/",
+    repositoryLink: "https://github.com/rodriguezmauricio/martz-todo-list-react",
+    bgImage: "https://meuwebdesigner.com.br/wp-content/uploads/2023/05/todo-list-min.jpg",
+    projectType: "personal",
+  },
+  {
+    projectName: "Barra Rio Serviços",
+    projectLink: "https://barrarioservicos.com.br/",
+    repositoryLink: "",
+    bgImage: "https://meuwebdesigner.com.br/wp-content/uploads/2023/05/barra-rio-min.jpg",
+    projectType: "real",
+  },
 ];
